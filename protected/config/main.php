f6e48bbdb09adf16069fbdd1d4a994b7321ad1d7
virgantara <vinux.edu@gmail.com>
2017-12-12 00:21:11 +0700

    rename to simpatika

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'SIMJAD | UNIDA Gontor',
+	'name'=>'SIMPATIKA | UNIDA Gontor',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
