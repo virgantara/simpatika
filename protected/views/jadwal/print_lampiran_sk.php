a1b8a4bee4d34c28e22851f0328603717a1492a1
virgantara <vinux.edu@gmail.com>
2017-12-12 12:07:56 +0700

    change Times

--- protected/views/jadwal/print_lampiran_sk.php
+++ protected/views/jadwal/print_lampiran_sk.php
@@ -1,7 +1,8 @@
 
+
 <br><br><br><br><br><br>
 
-<table width="100%" style="font-size: 10">
+<table width="100%" style="font-size: 10;font-family: 'Times'">
   <tr>
     <td width="10%">Lampiran:</td>
     <td width="90%"><?=$setting_sk->bunyi_lampiran;?>
@@ -27,14 +28,14 @@
   </tr>
 </table>
 <br><br>
-<table cellpadding="3" border="1" width="100%" style="font-size: 10">
+<table cellpadding="3" border="1" width="100%" style="font-size: 10;font-family: 'Times'">
   
   <thead>
     <tr>
-      <th width="16%"  style="text-align: center;"><strong>NIY</strong></th>
+      <th width="13%"  style="text-align: center;"><strong>NIY</strong></th>
       <th width="20%"  style="text-align: center;"><strong>NAMA<br>DOSEN</strong></th>
       <th width="35%"  style="text-align: center;"><strong>MATA KULIAH</strong></th>
-    <th width="7%"  style="text-align: center;"><strong>PRODI</strong></th>
+    <th width="10%"  style="text-align: center;"><strong>PRODI</strong></th>
     <th width="7%"  style="text-align: center;"><strong>SMTR</strong></th>
     <th width="10%"  style="text-align: center;"><strong>KELAS</strong></th>
     <th width="5%"  style="text-align: center;"><strong>SKS</strong></th>
@@ -52,10 +53,10 @@
     {
       ?>
         <tr>
-     <td width="16%" rowspan="<?=count($model)+1;?>" style="text-align: center;"><?=$m->kode_dosen;?></td>
+     <td width="13%" rowspan="<?=count($model)+1;?>" style="text-align: center;"><?=$m->kode_dosen;?></td>
       <td width="20%" rowspan="<?=count($model)+1;?>" style="text-align: center;"><?=$m->nama_dosen;?></td>
       <td width="35%"  style="text-align: center;"><?=$m->nama_mk;?></td>
-    <td width="7%"  style="text-align: center;"><?=$m->nama_prodi;?></td>
+    <td width="10%"  style="text-align: center;"><?=$m->nama_prodi;?></td>
     <td width="7%"  style="text-align: center;"><?=$m->semester;?></td>
     <td width="10%"  style="text-align: center;"><?=$kelas->nama_kelas;?></td>
     <td width="5%"  style="text-align: center;"><?=$m->SKS;?></td>
@@ -68,7 +69,7 @@
   ?>
    <tr>
       <td width="35%"  style="text-align: center;"><?=$m->nama_mk;?></td>
-    <td width="7%"  style="text-align: center;"><?=$m->nama_prodi;?></td>
+    <td width="10%"  style="text-align: center;"><?=$m->nama_prodi;?></td>
     <td width="7%"  style="text-align: center;"><?=$m->semester;?></td>
     <td width="10%"  style="text-align: center;"><?=$kelas->nama_kelas;?></td>
     <td width="5%"  style="text-align: center;"><?=$m->SKS;?></td>
@@ -81,7 +82,7 @@
    ?>
  </tbody>
 </table>
-<table width="100%" style="font-size: 10">
+<table width="100%" style="font-size: 10;font-family: 'Times'">
   <tr>
     <td width="45%">&nbsp;</td>
     <td width="55%" style="text-align: left">
