60e5fae0eb3cd7cbaeddc17687d7d40f9e242aba
virgantara <vinux.edu@gmail.com>
2017-12-12 19:09:17 +0700

    modif % kolom prodi k

--- protected/views/datakrs/admin.php
+++ protected/views/datakrs/admin.php
@@ -46,7 +46,7 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 	'filter'=>$model,
 	'columns'=>array(
 		// 'id',
-		'kode_fak',
+		// 'kode_fak',
 		'kode_prodi',
 		
 		'kode_mk',
@@ -56,6 +56,7 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 		'kode_dosen',
 		// 'namadosen',
 		'semester',
+		'tahun_akademik',
 		'kode_jadwal',
 		'kelas',
 		/*
