bb6da7f10fcc814b49c92f6390445535a8242f47
virgantara <vinux.edu@gmail.com>
2017-12-17 12:32:17 +0700

    add update pa

--- protected/views/mastermahasiswa/admin.php
+++ protected/views/mastermahasiswa/admin.php
@@ -50,6 +50,7 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 		'nim_mhs',
 		'nama_mahasiswa',
 		'nip_promotor',
+		'status_aktivitas',
 		/*
 		'nama_mahasiswa',
 		'tempat_lahir',
