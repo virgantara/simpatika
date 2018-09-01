7576284655ca339f1b7843d6d82aa1a61dcc8412
Oddy Virgantara Putra <oddy@unida.gontor.ac.id>
2018-09-01 14:01:11 +0700

    add cetak data ortu

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -274,7 +274,7 @@ class SiteController extends Controller
 	    ob_start();
 	    
 	    header('Content-Type: application/vnd.ms-excel');
-	    header('Content-Disposition: attachment;filename="dataortu.xls"');
+	    header('Content-Disposition: attachment;filename="dataortu_'.$mprodi->nama_prodi.'.xls"');
 	    header('Cache-Control: max-age=0');
 	    $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
 	    $objWriter->save('php://output');
