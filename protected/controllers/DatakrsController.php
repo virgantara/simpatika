2a35992fab7dcd6944941615792e56c310555754
Oddy <vinux.edu@gmail.com>
2019-06-29 11:16:32 +0700

    delete krs delete ekd

--- protected/controllers/DatakrsController.php
+++ protected/controllers/DatakrsController.php
@@ -110,8 +110,23 @@ class DatakrsController extends Controller
 	 */
 	public function actionDelete($id)
 	{
-		$this->loadModel($id)->delete();
+		$model = $this->loadModel($id);
 
+		$ekd = EkdJawaban::model()->findAllByAttributes(['simak_datakrs_id'=>$model->id]);
+		// print_r($ekd);exit;
+		if(!empty($ekd))
+		{
+			foreach($ekd as $m)
+			{
+				$m->delete();	
+			}
+			
+			
+		}
+
+		$model->delete();
+
+ 
 		// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
 		if(!isset($_GET['ajax']))
 			$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
---
+++ protected/models/EkdJawaban.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ * This is the model class for table "logs".
+ *
+ * The followings are the available columns in table 'logs':
+ * @property integer $id_log
+ * @property string $message
+ * @property string $created
+ */
+class EkdJawaban extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'simak_ekd_jawaban';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('jawaban', 'required'),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('id, simak_datakrs_id, ekd_kriteria_id, jawaban, created_at, updated_at', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+			'datakrs' => array(self::BELONGS_TO, 'Datakrs', 'simak_datakrs_id'),
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'jawaban' => 'Jawaban',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return Logs the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
