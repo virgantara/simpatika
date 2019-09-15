4477010bfc792a4fa2711b7b96709ae726670b21
Oddy <vinux.edu@gmail.com>
2019-09-15 15:42:09 +0700

    reset oauth

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -5,7 +5,7 @@
 <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta name="google-signin-client_id" content="668363583558-roc29ghfv67444rmi9sp1fvovpe68kn5.apps.googleusercontent.com">
+		<meta name="google-signin-client_id" content="668363583558-r1pp6okdumpn0k8h5lmqrn7noutbu2lp.apps.googleusercontent.com">
 		
    
 
