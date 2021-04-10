ff92cf90f89d0b0fafe5d1dde7f1cbd7e9d58188
virgantara <oddy@unida.gontor.ac.id>
2021-04-10 09:58:11 +0700

    fix

--- models/PenugasanSearch.php
+++ models/PenugasanSearch.php
@@ -42,7 +42,8 @@ class PenugasanSearch extends Penugasan
     public function search($params)
     {
         $query = Penugasan::find();
-
+        $query->alias('p');
+        $query->where(['p.NIY' => Yii::$app->user->identity->NIY]);
         // add conditions that should always apply here
 
         $dataProvider = new ActiveDataProvider([
